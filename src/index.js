0eacf6815aaf75a2ccb7972c79f545946f1d4460
B06505018 <young19990106@gmail.com>
2022-01-16 19:14:42 +0800

    Deploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme.js'
 import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
+// import reportWebVitals from './reportWebVitals.js';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
